6b21bde6818c58a3de11cacbc26fd01123851714
apachertr180navneeet <navneetgehlot03061993@gmail.com>
2024-07-05 10:23:47 +0530

    profile image

--- resources/views/admin/layouts/elements/header.blade.php
+++ resources/views/admin/layouts/elements/header.blade.php
@@ -22,7 +22,11 @@
 				<a class="nav-link dropdown-toggle hide-arrow" href="javascript:void(0);"
 					data-bs-toggle="dropdown">
 					<div class="avatar avatar-online">
-						<img src="{{asset('assets/admin/img/avatars/1.png')}}" alt="admin" class="w-px-40 h-auto rounded-circle" />
+						@if(Auth::user()->avatar)
+	    					<img src="{{asset(Auth::user()->avatar)}}" alt="admin" class="w-px-40 h-auto rounded-circle" />
+                        @else
+                            <img src="{{asset('assets/admin/img/avatars/1.png')}}" alt="admin" class="w-px-40 h-auto rounded-circle" />
+                        @endif
 					</div>
 				</a>
 				<ul class="dropdown-menu dropdown-menu-end">
